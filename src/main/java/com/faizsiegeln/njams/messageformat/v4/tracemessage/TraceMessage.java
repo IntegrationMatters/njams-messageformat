4b86736af10d07a7d9fcb42b29c67fe15756a84a
krautenberg <krautenberg@integrationmatters.com>
2019-04-04 15:41:32 +0200

    [SDK-132] Created the TraceMessage that can be sent to the server

---
+++ src/main/java/com/faizsiegeln/njams/messageformat/v4/tracemessage/TraceMessage.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2019 Faiz & Siegeln Software GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * The Software shall be used for Good, not Evil.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+package com.faizsiegeln.njams.messageformat.v4.tracemessage;
+
+import com.faizsiegeln.njams.messageformat.v4.common.CommonMessage;
+import com.faizsiegeln.njams.messageformat.v4.projectmessage.Tracepoint;
+
+/**
+ * @author krautenberg
+ * @version 4.0.1
+ */
+public class TraceMessage extends CommonMessage {
+
+    private String activityId;
+
+    private Tracepoint tracepoint;
+
+    public String getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(String activityId) {
+        this.activityId = activityId;
+    }
+
+    public Tracepoint getTracepoint() {
+        return tracepoint;
+    }
+
+    public void setTracepoint(Tracepoint tracepoint) {
+        this.tracepoint = tracepoint;
+    }
+}
