b0aebed1a86f1833546b67f85ac9d37ef41aee62
krautenberg <krautenberg@integrationmatters.com>
2019-10-15 09:44:42 +0200

    [MSG-4] Added default methods to pass the incoming instruction to call the correct onInstruction method. Returns true, if the correct class was found and false if not.

--- src/main/java/com/faizsiegeln/njams/messageformat/v4/command/wrapper/RequestListener.java
+++ src/main/java/com/faizsiegeln/njams/messageformat/v4/command/wrapper/RequestListener.java
@@ -1,11 +1,79 @@
 package com.faizsiegeln.njams.messageformat.v4.command.wrapper;
 
+import com.faizsiegeln.njams.messageformat.v4.command.Command;
+import com.faizsiegeln.njams.messageformat.v4.command.Instruction;
 import com.faizsiegeln.njams.messageformat.v4.command.wrapper.request.*;
 
 /**
  * This interface should be implemented in the SDK to process incoming instruction requests.
  */
 public interface RequestListener {
+
+    /**
+     * This calls the correct onInstruction(...Request) method if the command in the instruction has
+     * a corresponding Request class.
+     *
+     * @param instruction the incoming instruction
+     * @return true, if a corresponding request class was found and used, else false
+     * @throws NjamsMessageFormatException could be thrown while processing one of the onInstruction methods
+     */
+    default boolean onInstruction(Instruction instruction) throws NjamsMessageFormatException {
+        boolean foundCorrespondingRequestClass = true;
+        if (instruction != null) {
+            Command command = Command.getFromCommandString(instruction.getCommand());
+            if (command != null) {
+                switch (command) {
+                    case CONFIGURE_EXTRACT:
+                        onInstruction(new ConfigureExtractRequest(instruction));
+                        break;
+                    case DELETE_EXTRACT:
+                        onInstruction(new DeleteExtractRequest(instruction));
+                        break;
+                    case GET_EXTRACT:
+                        onInstruction(new GetExtractRequest(instruction));
+                        break;
+                    case GET_LOG_LEVEL:
+                        onInstruction(new GetLogLevelRequest(instruction));
+                        break;
+                    case GET_LOG_MODE:
+                        onInstruction(new GetLogModeRequest(instruction));
+                        break;
+                    case GET_TRACING:
+                        onInstruction(new GetTracingRequest(instruction));
+                        break;
+                    case RECORD:
+                        onInstruction(new RecordRequest(instruction));
+                        break;
+                    case REPLAY:
+                        onInstruction(new ReplayRequest(instruction));
+                        break;
+                    case SEND_PROJECTMESSAGE:
+                        onInstruction(new SendProjectMessageRequest(instruction));
+                        break;
+                    case SET_LOG_LEVEL:
+                        onInstruction(new SetLogLevelRequest(instruction));
+                        break;
+                    case SET_LOG_MODE:
+                        onInstruction(new SetLogModeRequest(instruction));
+                        break;
+                    case SET_TRACING:
+                        onInstruction(new SetTracingRequest(instruction));
+                        break;
+                    case TEST_EXPRESSION:
+                        onInstruction(new TestExpressionRequest(instruction));
+                        break;
+                    default:
+                        foundCorrespondingRequestClass = false;
+                        break;
+                }
+            } else {
+                foundCorrespondingRequestClass = false;
+            }
+        } else {
+            foundCorrespondingRequestClass = false;
+        }
+        return foundCorrespondingRequestClass;
+    }
 
     void onInstruction(ConfigureExtractRequest request);
 
--- src/main/java/com/faizsiegeln/njams/messageformat/v4/command/wrapper/ResponseListener.java
+++ src/main/java/com/faizsiegeln/njams/messageformat/v4/command/wrapper/ResponseListener.java
@@ -1,11 +1,79 @@
 package com.faizsiegeln.njams.messageformat.v4.command.wrapper;
 
+import com.faizsiegeln.njams.messageformat.v4.command.Command;
+import com.faizsiegeln.njams.messageformat.v4.command.Instruction;
 import com.faizsiegeln.njams.messageformat.v4.command.wrapper.response.*;
 
 /**
  * This interface should be implemented in the Server to receive processed instruction responses.
  */
 public interface ResponseListener {
+
+    /**
+     * This calls the correct onInstruction(...Response) method if the command in the instruction has
+     * a corresponding Response class.
+     *
+     * @param instruction the incoming instruction
+     * @return true, if a corresponding response class was found and used, else false
+     * @throws NjamsMessageFormatException could be thrown while processing one of the onInstruction methods
+     */
+    default boolean onInstruction(Instruction instruction) throws NjamsMessageFormatException {
+        boolean foundCorrespondingResponseClass = true;
+        if (instruction != null) {
+            Command command = Command.getFromCommandString(instruction.getCommand());
+            if (command != null) {
+                switch (command) {
+                    case CONFIGURE_EXTRACT:
+                        onInstruction(new ConfigureExtractResponse(instruction));
+                        break;
+                    case DELETE_EXTRACT:
+                        onInstruction(new DeleteExtractResponse(instruction));
+                        break;
+                    case GET_EXTRACT:
+                        onInstruction(new GetExtractResponse(instruction));
+                        break;
+                    case GET_LOG_LEVEL:
+                        onInstruction(new GetLogLevelResponse(instruction));
+                        break;
+                    case GET_LOG_MODE:
+                        onInstruction(new GetLogModeResponse(instruction));
+                        break;
+                    case GET_TRACING:
+                        onInstruction(new GetTracingResponse(instruction));
+                        break;
+                    case RECORD:
+                        onInstruction(new RecordResponse(instruction));
+                        break;
+                    case REPLAY:
+                        onInstruction(new ReplayResponse(instruction));
+                        break;
+                    case SEND_PROJECTMESSAGE:
+                        onInstruction(new SendProjectMessageResponse(instruction));
+                        break;
+                    case SET_LOG_LEVEL:
+                        onInstruction(new SetLogLevelResponse(instruction));
+                        break;
+                    case SET_LOG_MODE:
+                        onInstruction(new SetLogModeResponse(instruction));
+                        break;
+                    case SET_TRACING:
+                        onInstruction(new SetTracingResponse(instruction));
+                        break;
+                    case TEST_EXPRESSION:
+                        onInstruction(new TestExpressionResponse(instruction));
+                        break;
+                    default:
+                        foundCorrespondingResponseClass = false;
+                        break;
+                }
+            } else {
+                foundCorrespondingResponseClass = false;
+            }
+        } else {
+            foundCorrespondingResponseClass = false;
+        }
+        return foundCorrespondingResponseClass;
+    }
 
     void onInstruction(ConfigureExtractResponse response);
 
